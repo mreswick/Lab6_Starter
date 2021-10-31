34026e788c482c04bee7c5a7666a4465c01e16b9
mreswick <mreswick@ucsd.edu>
2021-10-30 20:34:46 -0700

    finished explore

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,7 +5,10 @@
 const recipes = [
   'https://introweb.tech/assets/json/ghostCookies.json',
   'https://introweb.tech/assets/json/birthdayCake.json',
-  'https://introweb.tech/assets/json/chocolateChip.json'
+  'https://introweb.tech/assets/json/chocolateChip.json',
+  'assets/recipes/recipe1.json',
+  'assets/recipes/recipe2.json',
+  'assets/recipes/recipe3.json'
 ];
 
 // Once all of the recipes that were specified above have been fetched, their
@@ -90,8 +93,13 @@ function createRecipeCards() {
 
   // Part 1 Expose - TODO
   for(let i = 0; i < Object.keys(recipeData).length; i++) {
+    // only create recipes for first 3 (that we already were given):
+    if(i >= 3) {
+      break;
+    }
+
     const recipeEl = document.createElement("recipe-card");
-    recipeEl.data = recipeData[recipes[i]]
+    recipeEl.data = recipeData[recipes[i]];
     // note to self that set data(data) isn't a function, so
     // you can't/don't use that syntax with it, but
     // rather use an assignment syntax.
@@ -114,4 +122,57 @@ function bindShowMore() {
   // in the recipeData object where you stored them/
 
   // Part 2 Explore - TODO
+
+  // website #1: https://marisamoore.com/maple-walnut-blueberry-crisp/
+  //  script is in: <script type="application/ld+josn" class="yoast-schema-graph">...</script>
+
+  // website #2: https://www.twopurplefigs.com/holiday-turkish-pasta-with-yogurt-sauce/
+  // script is in: <script type="application/ld+josn" class="yoast-schema-graph">...</script>
+
+  // https://cadryskitchen.com/vegan-strawberry-milk/
+  // script is in: <script type="application/ld+json" class="yoast-schema-graph">...</script>
+
+  let buttonEl = document.getElementsByTagName("button")[0];
+  buttonEl.addEventListener('click', butClickHandler);
+
+}
+
+function butClickHandler(event) {
+  let buttonEl = document.getElementsByTagName("button")[0];
+  if(buttonEl.textContent == "Show more") {
+    // change button content
+    buttonEl.innerHTML = "Show less";
+    // create and show elements for remaining recipes
+    for(let i = 3; i < Object.keys(recipeData).length; i++) {
+      const recipeEl = document.createElement("recipe-card");
+      recipeEl.data = recipeData[recipes[i]];
+      const mainEl = document.getElementsByTagName("main")[0];
+      mainEl.appendChild(recipeEl);
+    }
+  } else if(buttonEl.textContent == "Show less") {
+    // change button content
+    buttonEl.innerHTML = "Show more";
+    // delete elements for last three recipes
+    // note to self: assume below gets recipes in order they'd
+    // be in html page
+    const mainEl = document.getElementsByTagName("main")[0];
+    const recipeEls = document.getElementsByTagName("recipe-card");
+    console.log("recipeEls retrieved:");
+    console.log(recipeEls);
+    // index of 3 used as additional recipes we added in local .json
+    // files are #4-#6
+    for(let j = 3; j < Object.keys(recipeData).length; j++) {
+      console.log("in for loop to delete");
+      // note to self that this .remove() removes the element
+      // from the array as well as from the DOM,
+      // so need to keep index constant (as array length is decreasing
+      // with each delete)
+      recipeEls[3].remove();
+      console.log("Length of recipeEls:")
+      console.log(recipeEls.length);
+    }
+
+  } else {
+    console.log("Show more/less button error.");
+  }
 }
